d004cd98f0dc3f773e24bf87ee86c033c0c9fc38
Shpoodr <heyheylogan.m@gmail.com>
2026-04-08 18:16:44 -0700

    started on some code and made a scene

---
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerCombat : MonoBehaviour
+{
+    private PlayerControls _input;
+
+
+    [Header("References")]
+    //current Gauntlet
+    //both fist transform
+    //
+    [Header("StateFlags")]
+    public bool isAttacking;
+    public bool canCombo;
+    public bool isParry;
+    //need to add current attack node
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActivateHitbox()
+    {
+        
+    }
+
+    public void DeactivateHitbox()
+    {
+        
+    }
+
+    public void OpenComboWindow()
+    {
+        
+    }
+
+    public void EndAttack()
+    {
+        
+    }
+}
