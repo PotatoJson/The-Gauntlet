f249395898acaf3be7c9eb49b987a7f732e525db
Shpoodr <heyheylogan.m@gmail.com>
2026-05-11 15:45:15 -0700

    decoupled a lot of the stats and centralized them in the new PlayerStatsManager Script for easier access to each of the stats and changing them
    Also made some gem objects and linked them to the existing prefabs. I am going to create more later and make a game loop kind of script that handled xp emptying a chamber and so on which will just prompt the player with the reward menu.

--- Assets/Scripts/Player/ScriptableObjects/GemData.cs
+++ Assets/Scripts/Player/ScriptableObjects/GemData.cs
@@ -7,26 +7,3 @@ public abstract class GemData : BaseItemData
   public int GemTier;
   //potential socketing sound ADD HERE
 }
-
-[CreateAssetMenu(fileName = "New Stat Gem", menuName = "Items/Gems/Stat Gem")]
-public class StatGemData : GemData
-{
-    [Header("Stat Bonuses")]
-    public List<GemModifier> Modifiers = new List<GemModifier>();
-
-    [Header("Cursed Properties")]
-    public bool IsCursed;
-    //add more logic here when we have more info on curses
-}
-
-[CreateAssetMenu(fileName = "New Skill Gem", menuName = "Items/Gems/Skill Gem")]
-public class SkillGemData : GemData
-{
-    [Header("Skill Properties")]
-    public GameObject SkillPrefab;
-    public float BaseCooldown;
-    public float StaminaCost;
-
-    //example of general skill logic (see definitions script for list of categories)
-    public SkillCategory Category;
-}
