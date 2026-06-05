af13dec7688e205ef88d8b06f03f36ca3a3f93de
Shpoodr <heyheylogan.m@gmail.com>
2026-06-04 19:13:32 -0700

    Revert "some controller fixes and interaction menu fixes maybe"
    This reverts commit 3d674c8f638b7ebe0ffa4092a39eb26efd9ae52b.

--- Assets/Scripts/GauntletInteractable.cs
+++ Assets/Scripts/GauntletInteractable.cs
@@ -42,6 +42,8 @@ public class GauntletInteractable : MonoBehaviour
         if (InteractionPrompt != null)
         {
             _promptTransform = InteractionPrompt.transform;
+            _promptTransform.SetParent(null);
+            //_promptTransform.localScale = new Vector3(0.005f, 0.005f, 0.005f);
             InteractionPrompt.SetActive(false);
         }
 
